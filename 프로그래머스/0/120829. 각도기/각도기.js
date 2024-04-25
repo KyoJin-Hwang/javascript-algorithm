e3cb7889bc4e05c138fcec77fd1af696d2cae3c1
KyoJin <84490050+KyoJin-Hwang@users.noreply.github.com>
2024-04-25 16:09:37 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.js
+++ 프로그래머스/0/120829. 각도기/각도기.js
@@ -2,3 +2,8 @@ const solution = (angle) => angle <= 90 ?
       angle === 90 ? 2 : 1 
         : 
       angle === 180 ? 4 : 3 
+
+// filter 이용방법
+//  const solution = (angle) => {
+//   return [0, 90, 91, 180].filter(x => angle>=x).length;
+// }
