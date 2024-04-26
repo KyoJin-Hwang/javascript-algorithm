4fdf8018f278a2f1ecdf55252d7a380a798a811d
KyoJin <84490050+KyoJin-Hwang@users.noreply.github.com>
2024-04-26 15:58:50 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.js
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.js
@@ -2,3 +2,7 @@ function solution(numbers) {
     const answer = numbers.map(x=>x*2);
     return answer;
 }
+
+// function solution(numbers) {
+//    return numbers.reduce((a, b) => [...a, b * 2], []);
+// }
