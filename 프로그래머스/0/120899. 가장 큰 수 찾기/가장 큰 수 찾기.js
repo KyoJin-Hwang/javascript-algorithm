fdb6d557171ed7444901cd5d55e4344579371a1a
KyoJin <84490050+KyoJin-Hwang@users.noreply.github.com>
2024-05-13 18:36:40 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
@@ -8,3 +8,9 @@ function solution(array) {
     answer.push(array.indexOf(num,0))
     return answer
 }
+
+// 이게 조금 더 이쁜 코드^
+// function solution(array) {
+//     let max = Math.max(...array);
+//     return [max, array.indexOf(max)];
+// }
