778a2b50e8e658f2f0a129157dae5235318f7053
KyoJin <84490050+KyoJin-Hwang@users.noreply.github.com>
2024-05-14 18:46:41 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,13 +1,22 @@
+// 신박한코드 
+// function solution(num_list) {
+//     const answer = [0,0]
+//     for(let a of num_list){
+//         answer[a%2] += 1
+//     }
+//     return answer;
+// }
 function solution(num_list) {
-        let even = 0
-        let odd = 0
+    
+    let even = 0
+    let odd = 0
     num_list.map((el)=>{
-        if(el % 2 === 0){
+        if(el%2 === 0){
             even += 1
         }else{
             odd += 1
         }
-    } )
+    })    
     
     return [even, odd];
 }
