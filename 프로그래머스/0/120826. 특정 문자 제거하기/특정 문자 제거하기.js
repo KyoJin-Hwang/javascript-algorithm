994727c0b2c335b7f3016c8578d27c27517fe443
KyoJin <84490050+KyoJin-Hwang@users.noreply.github.com>
2024-05-24 20:47:09 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -1,7 +1,16 @@
+
+
+
 function solution(my_string, letter) {
-    let answer = '';
-    for(let idx in my_string){
-        answer += my_string[idx] !== letter ? my_string[idx] : ""
-    }
+    const answer = my_string.split(letter).join('')
+    
     return answer;
 }
+// 삼항연산자
+// function solution(my_string, letter) {
+//     let answer = '';
+//     for(let idx in my_string){
+//         answer += my_string[idx] !== letter ? my_string[idx] : ""
+//     }
+//     return answer;
+// }
