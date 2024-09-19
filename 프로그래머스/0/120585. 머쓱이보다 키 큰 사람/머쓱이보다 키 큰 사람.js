88d7d2b3b8fa80da187922cf4035fa2a61dbb127
Owen <owen1127@naver.com>
2024-09-19 16:59:19 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -1,5 +1,7 @@
 const solution = (array, height) => {
-  let answer = 0;
-  answer = array.filter(tall => tall > height)
-  return answer.length;
+
+ return array.filter(tall => tall > height).length
 }
+
+// 설명
+// filter 배열함수로 배열을 순회하며 머쓱이의 키보다 큰것들만 배열에서 반환하여 길이를 return 해준다. 
