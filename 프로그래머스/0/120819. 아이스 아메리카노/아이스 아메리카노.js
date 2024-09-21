855a313991c9102123f445599128d55f5356d276
Owen <owen1127@naver.com>
2024-09-21 19:56:21 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.js
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.js
@@ -6,6 +6,8 @@ function solution(money) {
     return answer;
 }
 
-// function solution(money) {
-//     return [Math.floor(money / 5500), money % 5500];
-// }
+// 설명
+// 1. americano 5500원 변수생성 
+// 2. glass 변수생성 => 전체 Money에서 americano 값을 나누고 소수점이 생길수 있으므로 floor 함수사용
+// 3. change 변수생성 => 전체 Money 에서 americano 를 나눈 나머지 값을 저장 
+// 4. answer 빈배열 안에 glass, change를 넣어준 이후 return 한다. 
