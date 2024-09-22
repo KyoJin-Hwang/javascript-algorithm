09d56514b306b3e31af5d3304aee0eeba857c319
Owen <owen1127@naver.com>
2024-09-22 18:48:28 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
@@ -5,11 +5,17 @@ function solution(array) {
         num = num > i ? num : i
     }
     answer.push(num)
-    answer.push(array.indexOf(num,0))
+    answer.push(array.indexOf(num))
     return answer
 }
 
-// 이게 조금 더 이쁜 코드^
+// 설명
+// answer 빈배열 변수 및 num 0 변수 생성
+// 1. for of 문으로 array 를 순회한다. 
+// 2. 큰값 끼리 비교하여 num 에 계속 추가해주고 마지막에 가장 큰값을 answer 에 push 한다. 
+// 3. 그리고 가장 큰값의 num 은 array.indexOf 를 활용하여 인덱스 위치를 찾는다. 
+
+// 배열에 요소가 너무 많은면 실패하거나 잘못된 결과를 가져올 수 있다. 
 // function solution(array) {
 //     let max = Math.max(...array);
 //     return [max, array.indexOf(max)];
