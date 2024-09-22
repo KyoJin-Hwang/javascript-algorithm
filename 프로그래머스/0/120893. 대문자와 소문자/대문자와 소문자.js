532718b2f999c16d7389b29ad3bc6ea9b7c73cf1
Owen <owen1127@naver.com>
2024-09-22 18:39:47 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.33 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -1,12 +1,17 @@
 function solution(my_string) {
-let arr = "" 
+let result = "" 
 
 for (let i of my_string) { 
 	if (i === i.toUpperCase()) {
-		arr += i.toLowerCase() 
+		result += i.toLowerCase() 
 	} else { 
-		arr += i.toUpperCase() 
+		result += i.toUpperCase() 
 	}
 }
-    return arr
+    return result
 }
+// 설명 
+// 1. 빈텍스트를 가진 result 변수 생성
+// 2. for of 문을 하용하여 string.length 만큼 순회한다. 
+// 3. if 문을 사용하여 대문자로 변경하였을때 같다면 소문자로 변경하고 
+// 4. else 문을 사용하여 소문자는 대문자로 변경한다. 
