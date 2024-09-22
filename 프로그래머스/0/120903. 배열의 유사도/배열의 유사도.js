f1df0912303f015d6de1e4f64ca89d832b09a1ba
Owen <owen1127@naver.com>
2024-09-22 19:31:13 +0900

    [level 0] Title: 배열의 유사도, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
@@ -3,3 +3,8 @@ function solution(s1, s2) {
     answer = s1.filter((e) => s2.includes(e)).length
     return answer;
 }
+
+// 설명 
+// answer 초기값 0 변수 생성
+// s1 배열을 filter 활용하여 배열 순회를 하고 s2.includes에서 s1의 e 값이 같은것만 filter 후 
+// filter 된 배열의 길이만큼 answer에 저장 
