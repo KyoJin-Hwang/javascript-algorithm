10a81cecdcb431fc0aec2fe3b1689120de0bf960
Owen <owen1127@naver.com>
2024-09-22 19:43:16 +0900

    [level 0] Title: 세균 증식, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120910. 세균 증식/세균 증식.js
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.js
@@ -4,3 +4,12 @@ function solution(n, t) {
   }
     return n;
 }
+
+// 설명 
+// let 은 1부터 시작하며 t 까지 반복한다 
+// n에다가 *= 2 로 두배만큼 값을 넣어준후 return 
+
+// reduce 활용
+// function solution(n, t) {
+//   return [...Array(t)].reduce((acc) => acc * 2, n);
+// }
