ab9544bfc2e7f73db4b0b56e4fe07705748040c9
Owen <owen1127@naver.com>
2024-09-23 18:12:22 +0900

    [level 0] Title: 모음 제거, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.js
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.js
@@ -1,6 +1,10 @@
 function solution(my_string) {
+    // 모음 변수 생성
     const str_filter = 'aeiou'
+    // my_string 을 split 하여 배열로 만든다.
+    // filter로 모음에 포함 되는것을 제외한 나머지는 반환한다.
+    // join 으로 string 배열을 string 변환하여 반환한다. 
     let newString = my_string.split('').filter((el)=>(!str_filter.includes(el))).join('')
     return newString
-    
 }
+
