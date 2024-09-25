9f66488ee2d754cba339a88a152e056a7d87447e
Owen <owen1127@naver.com>
2024-09-25 17:00:54 +0900

    [level 0] Title: 수 조작하기 1, Time: 6.86 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
@@ -1,16 +1,16 @@
+const operations = {
+  w: (n) => n + 1,
+  s: (n) => n - 1,
+  d: (n) => n + 10,
+  a: (n) => n - 10,
+};
+
 function solution(n, control) {
-    let answer = 0;
-    [...control].map((el)=> {
-        if(el === 'w'){
-            n += 1 
-        }else if(el === 's'){
-            n -= 1 
-        }else if(el === 'd'){
-            n += 10
-        }else{
-            n -= 10
-        }
-        return answer = n 
-    })
-    return answer;
+  return [...control].reduce((prev, op) => {
+    return operations[op](prev); 
+  }, n); 
 }
+
+// 설명
+// 객체를 선언하여 각규칙에 맞는 함수를선언한다 
+// reduce 를 사용하여 operations에 맞는 idx (control되는)에 함수를 가져와서 사용하여 더해준다. 
