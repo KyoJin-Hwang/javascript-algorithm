76c4641e88992d2b481b86208fe48c4ce29aaa49
Owen <owen1127@naver.com>
2024-09-25 16:09:07 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.22 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.js
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.js
@@ -1,5 +1,17 @@
 function solution(n, k) {
     let answer = [];
-    answer = Array.from(Array(Math.floor(n/k)), (el,idx)=> k * (idx+1) )
+    for(let i = k ; i <= n; i+=k){
+        answer.push(i)
+    }
+    // answer = Array.from(Array(Math.floor(n/k)), (el,idx)=> k * (idx+1) )
     return answer;
 }
+
+// for 문 설명 
+// 1. i 는 k의 배수 숫자 로 시작되고 
+// 2. i 는 n보다 작거나 같아야한다 또한 i의 증감은 +=k 로 k의 배수만큼 숫자가 증가하게 만든다.
+// 3. answer 에 push 해준다. 
+
+// Array from 화살표함수 
+// 1. Array.from 배열초기화 및 Array(Math.floor(n/k))로 배열 length를 정해준다. 
+// 2. idx 를 사용하여 k (배수) * idx+1 을 곱하여 배수가 증가 할 수 있게 만들어준다.
