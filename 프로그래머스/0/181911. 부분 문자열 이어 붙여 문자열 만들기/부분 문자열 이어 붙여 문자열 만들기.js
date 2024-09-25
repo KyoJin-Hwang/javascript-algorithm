281dd3bc1541a57d1feb12d4bbb65c14249be613
Owen <owen1127@naver.com>
2024-09-25 16:34:21 +0900

    [level 0] Title: 부분 문자열 이어 붙여 문자열 만들기, Time: 0.12 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.js
+++ 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.js
@@ -4,3 +4,8 @@ function solution(my_strings, parts) {
     answer = my_strings.map((el,idx)=> el.slice(parts[idx][0], parts[idx][1]+1))
     return answer.join("");
 }
+
+// 설명 
+// mystrings 를 map으로 순회하며 각 string 마다 slice 한다. 
+// 이때 slice 할때 parts 는 이차원배열이므로 첫번째에 [idx]만큼 명시하며 
+// 마지막에 join 으로 텍스트를 합쳐준다. 
