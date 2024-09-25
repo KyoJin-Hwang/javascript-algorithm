4e1418acc3b2f214f55c9b94c5c2aa0f2db6874a
Owen <owen1127@naver.com>
2024-09-26 03:38:19 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -13,3 +13,11 @@ function solution(n) {
     answer.push(1)
     return answer
 }
+
+// 재귀함수
+// function solution(n, arr = []) {
+//     arr.push(n)
+//     if (n === 1) return arr
+//     if (n % 2 === 0) return solution(n / 2, arr)
+//     return solution(3 * n + 1, arr)
+// }
