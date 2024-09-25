bed58a07e2c22eeb65994ce8b97fcef9a814d13d
Owen <owen1127@naver.com>
2024-09-25 21:24:49 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -9,9 +9,15 @@ function solution(num_list) {
     return odd > even ? odd : even; 
 }
 
+
 // 설명 
 // 홀수 및 짝수 변수 선언
 // num_list 를 map 으로 순회하며 idx 가 % 2 했을때 0이나오면 not 연산자를 통해 
 // even 변수에 num_list idx 에 맞는 값을 더해준다 .
 // odd 는 0 이 아닐때의 나머지가 있으면 false 로 전환하여 odd 변수에 값을 더해준다
-// return 에서 더 큰것으로 삼항연산자 계산을하여 그값을 return 한다 
+// return 에서 더 큰것으로 삼항연산자 계산을하여 그값을 return 한다
+
+// reduce 풀이
+// function solution(num_list) {
+    // return Math.max(...num_list.reduce(([o, e], c, i) => i % 2 ? [o + c, e] : [o, e + c], [0, 0]));
+// }
