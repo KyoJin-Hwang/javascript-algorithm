21b25b66f9e38c5a604681b01071e2018a0533aa
Owen <owen1127@naver.com>
2024-09-25 16:46:10 +0900

    [level 0] Title: 글자 이어 붙여 문자열 만들기, Time: 5.56 ms, Memory: 34.8 MB -BaekjoonHub

--- 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.js
+++ 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.js
@@ -1,5 +1,6 @@
 function solution(my_string, index_list) {
-    
-    
     return index_list.map((el,idx)=> [...my_string][el]).join("");
 }
+
+// 설명 
+// index_list 를 map으로 순회하여 my_string에 해당하는 idx위치를 찾아가 index_list 값을 바꿔준다.
