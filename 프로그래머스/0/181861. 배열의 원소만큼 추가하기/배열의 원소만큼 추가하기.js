402320e4ee69482c40183881143dd0dddea161dc
Owen <owen1127@naver.com>
2024-09-26 03:13:44 +0900

    [level 0] Title: 배열의 원소만큼 추가하기, Time: 3.59 ms, Memory: 39.6 MB -BaekjoonHub

--- 프로그래머스/0/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.js
+++ 프로그래머스/0/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.js
@@ -1,11 +1,16 @@
+// function solution(arr) {
+//     let answer = [];
+
+//     arr.map((el)=>{
+//         for(let i = 0; i<el; i++){
+//             answer.push(el)
+//         }
+//     })
+
+//     return answer;
+// }
+
+
 function solution(arr) {
-    let answer = [];
-
-    arr.map((el)=>{
-        for(let i = 0; i<el; i++){
-            answer.push(el)
-        }
-    })
-
-    return answer;
+    return arr.reduce((list, num) => [...list, ...Array(num).fill(num)], []);
 }
