4ad6841da2d52eb8babc229f513c3d08037685c4
Owen <owen1127@naver.com>
2024-09-26 15:15:10 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.25 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
@@ -1,6 +1,7 @@
 function solution(n) {
     let arr = Array.from(Array(n), () => Array(n).fill(0)) ;
-    return arr.map((arr2, idx)=>{
-        return arr2.map((el, idx2)=> idx2 === idx ? 1 : 0)
-    })
+   for (let i = 0; i < n; i++) {
+    arr[i][i] = 1;
+  }
+    return arr 
 }
