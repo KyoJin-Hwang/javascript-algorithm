f284caa8bc1e5377cb00f8b0ae325d7eef62ac71
Owen <owen1127@naver.com>
2024-09-26 17:29:35 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    let answer = 0 
+    if (n%2===0){
+        for(let i=2; i<=n; i+=2){
+            answer+=i*i
+        }
+    } else {
+        for (let i=1; i<=n; i+=2){
+            answer+=i
+        }
+    }
+    return answer
+}
