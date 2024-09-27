fc228acaf020067dd863ae545d950984e8246702
Owen <owen1127@naver.com>
2024-09-27 15:12:42 +0900

    [level 0] Title: 세 개의 구분자, Time: 27.80 ms, Memory: 60.7 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.js
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.js
@@ -2,3 +2,6 @@ function solution(myStr) {
     const result = myStr.split(/[a|b|c]/g).filter((a)=> a)
     return result.length ? result : ["EMPTY"]
 }
+
+// match 활용
+// const solution=s=>s.match(/[^a-c]+/g)||['EMPTY'] 
