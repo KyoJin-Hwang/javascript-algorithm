e92e0ff0a23bc5b9b0b0a8e57531481d376a3f15
Owen <owen1127@naver.com>
2024-09-27 17:45:21 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.18 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,8 @@
+function solution(num) {
+if (num === 1) return 0;
+    for(let i=1; i<500; i++) {
+        num = num % 2 === 0 ? num / 2 : num * 3 + 1;
+        if(num === 1) return i;
+    }
+    return -1;
+}
