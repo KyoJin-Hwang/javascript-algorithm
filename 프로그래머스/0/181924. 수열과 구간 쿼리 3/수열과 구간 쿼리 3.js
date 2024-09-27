b0d125ff012b563d978cd6c101d430fb9e60c641
Owen <owen1127@naver.com>
2024-09-27 15:04:42 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.90 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
@@ -1,10 +1,6 @@
 function solution(arr, queries) {
-    for (let i = 0; i < queries.length; i++) {
-        const [start, end] = queries[i];
-        
-        let temp = arr[start];
-        arr[start] = arr[end];
-        arr[end] = temp;
-    }
+    queries.forEach( ([a,b]) => {
+        [arr[a],arr[b]] = [arr[b],arr[a]];
+    })
     return arr;
 }
