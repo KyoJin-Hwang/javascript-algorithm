f6b19c9bd27cd33eedb1c32bbbeb14be74834f34
Owen <owen1127@naver.com>
2024-09-29 20:35:26 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,7 +1,6 @@
 function solution(numbers, answer = 0) {
     for(let i = 0; i < 10; i++){
-        numbers.includes(i)  ? i : answer += i
-        
+        if(!numbers.includes(i)) answer += i
     }
     return answer;
 }
