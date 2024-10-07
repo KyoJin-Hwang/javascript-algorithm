51fd8bd2ea6b9c6dec67f801591ecad95f1bafd4
Owen <owen1127@naver.com>
2024-10-07 15:45:38 +0900

    [level 2] Title: 타겟 넘버, Time: 15.20 ms, Memory: 35.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,19 +1,15 @@
 function solution(numbers, target) {
-  let answer = 0;
-  const length = numbers.length;
-
-  function dfs(count, sum) {
-    if (count === length) {
-      if (target === sum) {
-        answer++;
-      }
-      return;
+    let answer = 0;
+    getAnswer(0,0);
+    function getAnswer(x,value) {
+        if(x<numbers.length){
+            getAnswer(x+1,value + numbers[x]);
+            getAnswer(x+1,value - numbers[x]);
+        } else{
+            if(value === target){
+                answer++
+            }
+        }
     }
-
-    dfs(count + 1, sum + numbers[count]);
-    dfs(count + 1, sum - numbers[count]);
-  }
-
-  dfs(0, 0);
-  return answer
+    return answer;
 }
