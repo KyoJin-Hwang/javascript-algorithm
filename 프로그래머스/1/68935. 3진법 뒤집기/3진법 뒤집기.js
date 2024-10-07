a77fd90a9fdd8c980f0a7280287dddcc8dc6ded0
Owen <owen1127@naver.com>
2024-10-07 14:07:29 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,7 +1,3 @@
 function solution(n) {
-    let answer = 0;
-    const reverse3 = n.toString(3).split("").reverse().join("")
-    answer = parseInt(reverse3, 3)
-
-    return answer;
+    return parseInt([...n.toString(3)].reverse().join(""), 3)
 }
