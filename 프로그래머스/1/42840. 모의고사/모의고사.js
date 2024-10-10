6536831206d5eaff65112dc566847788df9d1abf
Owen <owen1127@naver.com>
2024-10-10 13:42:02 +0900

    [level 1] Title: 모의고사, Time: 18.17 ms, Memory: 38.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -0,0 +1,23 @@
+function solution(answers) {
+    const patterns = [
+        [1,2,3,4,5],[2,1,2,3,2,4,2,5],[3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    ]
+    let scores = [0,0,0];
+    for(const [i, answer] of answers.entries()){
+        for(const [j, pattern] of patterns.entries()){
+            if(answer === pattern[i % pattern.length]){
+                scores[j] += 1
+            }
+        }
+    }
+    const max = Math.max(...scores)
+    
+    const high = []
+    
+    for(let i = 0; i < scores.length; i++){
+        if(scores[i] === max){
+            high.push(i+1)
+        }
+    }
+    return high
+}
