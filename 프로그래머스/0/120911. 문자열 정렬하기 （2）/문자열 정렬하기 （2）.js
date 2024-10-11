0f3289682cabee18faecaabb730216933615cb5c
Owen <owen1127@naver.com>
2024-10-11 16:47:00 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.js
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.js
@@ -1,5 +1,5 @@
 function solution(my_string) {
     var answer = '';
     
-    return my_string.toLowerCase().split("").sort((a,b)=> a > b ? 1 : -1).join("")
+    return my_string.toLowerCase().split("").sort().join("")
 }
