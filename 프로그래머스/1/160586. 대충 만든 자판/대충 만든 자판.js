5b808f3d2d5226f4b772fb24cd58a034514afd5b
Owen <owen1127@naver.com>
2024-10-22 17:36:03 +0900

    [level 1] Title: 대충 만든 자판, Time: 8.55 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
@@ -2,29 +2,30 @@ function solution(keymap, targets) {
     let answer = [];
     
     for (let i = 0; i < targets.length; i++) {
-        let totalPresses = 0;  // 각 타겟에 대한 총 누적 횟수
+        let totalPresses = 0;  
+        
         let arr = [...targets[i]];
         
         for (let j = 0; j < arr.length; j++) {
-            let minPress = Infinity;  // 최소 눌림 수를 무한대로 초기화
+            let minPress = Infinity;  
             
             for (let k = 0; k < keymap.length; k++) {
                 let val = keymap[k].indexOf(arr[j]);
                 
                 if (val !== -1) {
-                    minPress = Math.min(minPress, val + 1); // +1은 키의 위치를 0부터가 아닌 1부터 카운트하기 위해서
+                    minPress = Math.min(minPress, val + 1); 
                 }
             }
             
             if (minPress === Infinity) {
-                totalPresses = -1;  // 찾을 수 없는 경우 -1로 설정
-                break;  // 이 문자는 찾을 수 없으므로 다음 타겟으로 넘어감
+                totalPresses = -1;  
+                break;  
             } else {
-                totalPresses += minPress;  // 최소 눌림 수를 총 횟수에 더함
+                totalPresses += minPress;  
             }
         }
         
-        answer.push(totalPresses);  // 각 타겟의 누적 횟수를 answer에 추가
+        answer.push(totalPresses);  
     }
     
     return answer;
