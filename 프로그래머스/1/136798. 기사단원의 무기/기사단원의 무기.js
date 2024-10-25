9865d1311248bf53ca3f985e31f1e6b4aa6f10b7
Owen <owen1127@naver.com>
2024-10-25 14:44:51 +0900

    [level 1] Title: 기사단원의 무기, Time: 42.95 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
@@ -1,16 +1,19 @@
 function solution(number, limit, power) {
-  let answer = 0;
+    // 약수 저장할 배열 ( idx 맞추기위한 +1 )
+  const counts = Array(number + 1).fill(0);
 
-  for (let k = 1; k <= number; k++) {
-    let count = 0;
-    for (let i = 1; i * i <= k; i++) {
-        
-      if (k % i === 0) {
-        count++;
-        if (k / i != i) count++;
-      }
+  for (let i = 1; i <= number; i++) {
+    for (let j = i; j <= number; j += i) {
+      counts[j]++;  // i의 배수에 대해 약수 개수 추가
     }
-    answer += limit < count ? power : count;
   }
+
+  let answer = 0;
+  
+  // 0 번째 인덱스는 0 k는 1부터시작 
+  for (let k = 1; k <= number; k++) {
+    answer += counts[k] > limit ? power : counts[k];
+  }
+
   return answer;
 }
