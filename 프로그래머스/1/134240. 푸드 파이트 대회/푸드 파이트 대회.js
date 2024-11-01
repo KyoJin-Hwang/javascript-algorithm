41bb57a2c733456e4dd35ca1c8f339c231c05641
Owen <owen1127@naver.com>
2024-11-01 17:15:20 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.25 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -2,7 +2,6 @@ function solution(food) {
     let res = '';
     for (let i = 1; i < food.length; i++) {
         res += String(i).repeat(Math.floor(food[i]/2));
-        
     }
 
     return res + '0' + [...res].reverse().join('');
