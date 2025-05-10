c5f71734cd244c2223698f66a8eeee8a02dabe15
Owen <owen1127@naver.com>
2025-05-10 13:23:16 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 3.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/README.md
+++ 프로그래머스/0/120824. 짝수 홀수 개수/README.md
@@ -1,10 +1,10 @@
 # [level 0] 짝수 홀수 개수 - 120824 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120824) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120824?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.07 ms
+메모리: 33.6 MB, 시간: 3.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 23일 18:07:28
+2025년 05월 10일 13:23:12
 
 ### 문제 설명
 
--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,27 +1,9 @@
 function solution(num_list) {
-    // 변수 생성
-    const answer = [0,0]
-    
-    // Map 함수를 사용하여 배열 순회
-    num_list.map((el)=>{
-        if(el%2 === 0){
-        // el%2 === 0 짝수일경우 answer[0]에 증감연산자
-            answer[0]++
-        }else{
-        // 홀수일경우 answer[1]에 증감연산자
-            answer[1]++
-        }
-    })
+    const answer = new Array(2).fill(0)
+    console.log(answer)
+    for(const num of num_list){
+        if(num % 2 === 0) answer[0] += 1
+        else answer[1] += 1
+    }
     return answer;
 }
-
-
-
-// 신박한코드 
-// function solution(num_list) {
-//     const answer = [0,0]
-//     for(let a of num_list){
-//         answer[a%2]++
-//     }
-//     return answer;
-// }
