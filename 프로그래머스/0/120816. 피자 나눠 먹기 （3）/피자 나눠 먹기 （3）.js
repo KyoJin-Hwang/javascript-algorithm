4745ef745525df714410df85b8be63597f84f162
Owen <owen1127@naver.com>
2025-05-10 13:29:37 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.js
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.js
@@ -1,8 +1,6 @@
 function solution(slice, n) {
-    let answer = Math.ceil(n/slice);
+    let answer = 0 
+    answer = Math.ceil(n / slice)
     
     return answer;
 }
-
-// 설명
-// 인원수/조각 하여 ceil 함수로 올림 처리하여 result 값을 return 해준다
