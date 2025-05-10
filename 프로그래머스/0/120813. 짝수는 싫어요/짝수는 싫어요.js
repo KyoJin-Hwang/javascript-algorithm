cd02a80b2667e22bab0d5fe04f523282cc228c83
Owen <owen1127@naver.com>
2025-05-10 15:23:45 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -1,9 +1,7 @@
 function solution(n) {
-    const answers = [];
-    for (let i = 1; i <= n; i+=2) answers.push(i)
-    return answers;
+    let answer = [];
+    for(let i = 1; i <= n; i+=2){
+        answer.push(i)
+    }
+    return answer;
 }
-
-// 설명
-// 1. 빈배열을 선언 
-// 2. 1부터 시작하는 for문으로  +=2 로 홀수 만큼 돌려준다. 그리고 n보다 작거나 같게하여 빈배열에 값추가
