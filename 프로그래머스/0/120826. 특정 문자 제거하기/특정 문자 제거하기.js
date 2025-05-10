3e0fb60b35af9d6bfb2fb8530d80fdef68677a25
Owen <owen1127@naver.com>
2025-05-10 13:25:01 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -1,16 +1,3 @@
-
-
-
 function solution(my_string, letter) {
-    const answer = my_string.split(letter).join('')
-    
-    return answer;
+    return my_string.replaceAll(letter, "");
 }
-// 삼항연산자
-// function solution(my_string, letter) {
-//     let answer = '';
-//     for(let idx in my_string){
-//         answer += my_string[idx] !== letter ? my_string[idx] : ""
-//     }
-//     return answer;
-// }
