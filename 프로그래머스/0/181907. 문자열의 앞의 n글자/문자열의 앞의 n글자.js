f33834c07734074b93b43438c1ecb6bab440d233
Owen <owen1127@naver.com>
2025-05-10 14:54:46 +0900

    [level 0] Title: 문자열의 앞의 n글자, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.js
+++ 프로그래머스/0/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.js
@@ -1,7 +1,4 @@
 function solution(my_string, n) {
-    let answer = my_string.slice(0,n);  
-    return answer;
-}
 
-// 설명
-// slice 로 텍스트 n 만큼 자른다. 
+    return [...my_string].splice(0, n).join('');
+}
