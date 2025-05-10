323f87b1a2004526ffb43a2dce6beb5ef8103def
Owen <owen1127@naver.com>
2025-05-10 15:03:23 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
@@ -1,7 +1,3 @@
 function solution(array, n) {
-  return array.filter(item => item === n).length;
+    return array.filter((num)=> num === n).length;
 }
-
-// 설명
-// 1. filter 로 배열을 순회하여 n과 같은 값만 반환한다. 
-// 2. filter로 새롭게 반환한 배열의 길이를 return 해준다. 
