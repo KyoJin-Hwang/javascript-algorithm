2cc6cb6f4123b20e4579a5d06993d452523b5f76
Owen <owen1127@naver.com>
2025-05-13 00:44:55 +0900

    Title: 문자 출력하기, Time: 169ms, Memory: 15MB, Status: Passed - Codetree

--- 250513/문자 출력하기/print-text.js
+++ 250513/문자 출력하기/print-text.js
@@ -1,6 +1,9 @@
+const fs = require('fs')
+const str = fs.readFileSync(0).toString().trim()
+
+
 for(let i = 1; i <= 8; i++){
-    
-    if(i === 8) console.log('G'.repeat(i))
+    if(i === 8) console.log(str.repeat(i))
 }
 
 
