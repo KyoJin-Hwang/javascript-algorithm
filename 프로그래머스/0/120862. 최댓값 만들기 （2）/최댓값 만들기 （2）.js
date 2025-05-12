e40437097f91ff8e0270c0189db66973249460a8
Owen <owen1127@naver.com>
2025-05-12 14:31:22 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,7 +1,9 @@
 function solution(numbers) {
-    let size = numbers.length;
-    
-    numbers.sort((a,b) => a - b);
-    
-    return Math.max(numbers[0] * numbers[1], numbers[size - 1] * numbers[size- 2]);
+    numbers.sort((a, b) => a - b); // 오름차순 정렬
+
+    const n = numbers.length;
+    const max1 = numbers[n - 1] * numbers[n - 2]; // 가장 큰 수 2개
+    const max2 = numbers[0] * numbers[1];         // 가장 작은 수 2개
+
+    return Math.max(max1, max2);
 }
