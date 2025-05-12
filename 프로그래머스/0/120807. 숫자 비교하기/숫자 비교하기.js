9a90eb509b67ba751eabcae4eddfc9acd669df25
Owen <owen1127@naver.com>
2025-05-12 14:11:26 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.js
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.js
@@ -1,5 +1,4 @@
-const solution = (num1, num2) => num1 === num2 ? 1 : -1
-
-// 설명
-// 1. num1과 num2를 비교 연산자 및 삼항연사자 를 사용한다. 
-// 2. true 일경우 1 false 일경우 -1 로 return 한다.
+function solution(num1, num2) {
+    
+    return num1 === num2 ? 1 : -1;
+}
