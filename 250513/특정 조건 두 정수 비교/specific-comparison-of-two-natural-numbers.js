5a7c1400134de06358b35fa3c1649d7478cf19f9
Owen <owen1127@naver.com>
2025-05-13 00:25:45 +0900

    Title: 특정 조건 두 정수 비교, Time: 168ms, Memory: 15MB, Status: Wrong Answer - Codetree

--- 250513/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.js
+++ 250513/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
-const input = fs.readFileSync(0).toString().trim().split(' ').map(el=> ~~el)
+const input = fs.readFileSync(0).toString().trim().split(' ').map(el=> Number(el))
 
 const [a,b] = input
 
