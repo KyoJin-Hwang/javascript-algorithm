0035d1fab17e6c07a3894e988cd5afdfd52ba822
Owen <owen1127@naver.com>
2025-05-12 13:05:38 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.js
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -1,13 +1,7 @@
 function solution(dot) {
-    const [num,num2] = dot;
-    const check = num * num2 > 0;
-    
-    return num > 0 ? (check ? 1 : 4) : (check ? 3 : 2);
+    const [num1, num2] = dot
+    const check = num1 * num2 
+    return num1 > 0 ? 
+        check > 0 ? 1 : 4 
+        : check > 0 ? 3 : 2;
 }
-
-// 설명 
-// 1. num, num2 구조분해할당
-// 2. check는 음수 양수 체크를 위한 변수 
-// 3. num 삼항연산자 기준으로 x 의 양수 음수를 나눠준다. 
-//    x 양수 : 1,4  x 음수 : 3, 2 
-// 4. check 삼항연산자 true 일때 양수 false 일때 음수  
