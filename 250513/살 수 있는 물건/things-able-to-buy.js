89fc73d95b05dd00e98802fc556b2ed470230460
Owen <owen1127@naver.com>
2025-05-13 00:38:17 +0900

    Title: 살 수 있는 물건, Time: 182ms, Memory: 15MB, Status: Passed - Codetree

--- 250513/살 수 있는 물건/things-able-to-buy.js
+++ 250513/살 수 있는 물건/things-able-to-buy.js
@@ -2,5 +2,5 @@ const fs = require('fs')
 const num = Number(fs.readFileSync(0).toString().trim())
 
 if(3000 <= num) console.log('book')
-else if(1000 <= num && 3000 > num) console.log('mask')
+else if(1000 <= num) console.log('mask')
 else console.log('no')
