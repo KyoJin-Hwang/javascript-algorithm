d35ade70ef14345919d798a2365c6ab9f2e20373
Owen <owen1127@naver.com>
2025-05-12 14:21:34 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.22 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -1,15 +1,5 @@
 function solution(my_string) {
-    let answer = 0;
-    my_string.split("").map(e => !isNaN(e)? answer += Number(e) : 0)
-    return answer;
+    const reg = /[0-9]/g;
+    return my_string.match(reg).reduce((acc, cur)=> acc + ~~cur, 0)
+    
 }
-
-// 전개연산자
-// function solution(my_string) {
-//     return [...my_string].reduce((acc,cur)=>Number(cur) ? +acc + +cur : acc, 0)
-// }
-
-// 설명 
-// 1. my_string 을 split 으로 각 배열로 쪼갠다. 
-// 2. map 함수를 이용하여 순회한다. 
-// 3. 순회 도중 isNaN을 활용하여 숫자인지 글자인지 판별 후 answer에 누적하여 return 해준다. 
