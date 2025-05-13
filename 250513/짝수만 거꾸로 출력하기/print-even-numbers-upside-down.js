4e0d9fcdc27ff702d58b71e8b812e64b3c4d5071
Owen <owen1127@naver.com>
2025-05-13 18:23:04 +0900

    Title: 짝수만 거꾸로 출력하기, Time: 170ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250513/짝수만 거꾸로 출력하기/print-even-numbers-upside-down.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+const arr = fs.readFileSync(0).toString().trim().split('\n')[1].split(' ').map(Number)
+
+let answer = []
+
+for(let i of arr){
+    if(i % 2 === 0) answer.push(i)
+}
+
+console.log(answer.join(' '))
