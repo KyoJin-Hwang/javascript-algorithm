5b0489f1624b1b1f94ab7fe76303881d45ece1be
Owen <owen1127@naver.com>
2025-05-15 14:50:52 +0900

    Title: 특정 수 맞히기, Time: 178ms, Memory: 15MB, Status: Wrong Answer - Codetree

--- 250515/특정 수 맞히기/catching-specific-number.js
+++ 250515/특정 수 맞히기/catching-specific-number.js
@@ -1,10 +1,12 @@
 const fs = require('fs')
-const input = fs.readFileSync(0).toString().trim().split('\n').mpa(Number)
+const input = fs.readFileSync(0).toString().trim().split('\n').map(Number)
 
 let idx = 0 
 
-while(input[idx] !== 25){
+while(input[idx]){
     if(input[idx] < 25) console.log('Higher')
     else if(input[idx] > 25) console.log('Lower')
+    else console.log('Good')
+    idx++
 }
 
