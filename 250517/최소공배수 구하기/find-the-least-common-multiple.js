580058ceec9298cb0a00fa95488c93df9b32f411
Owen <owen1127@naver.com>
2025-05-17 14:52:39 +0900

    Title: 최소공배수 구하기, Time: 163ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250517/최소공배수 구하기/find-the-least-common-multiple.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+let [n, m] = input[0].split(' ').map(Number);
+
+const gcd = (a,b) => b === 0 ? a : gcd(b, b%a)
+const lcm = (a,b) => (a * b) / gcd(a, b)
