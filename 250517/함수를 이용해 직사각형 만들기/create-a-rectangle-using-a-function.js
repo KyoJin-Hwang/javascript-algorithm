dc2e736988765bb6da672d3fc362085168746171
Owen <owen1127@naver.com>
2025-05-17 14:07:47 +0900

    Title: 함수를 이용해 직사각형 만들기, Time: 186ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250517/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+let [n, m] = input[0].split(" ").map(Number);
+
+
+for(let i = 0; i < n; i++){
+    let answer = ''
+    for(let j = 0; j < m; j++){
+        answer += '1'
+    }
+    console.log(~~answer)
+}
