2fa4a60a2d31ccb11a96a007ca266ac79bc95a51
Owen <owen1127@naver.com>
2025-05-17 14:05:17 +0900

    Title: 반복 출력하기, Time: 188ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250517/반복 출력하기/repeated-output.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const n = Number(input[0]);
+
+for(let i = 0; i < n; i++){
+    console.log(`12345^&*()_`)
+}
