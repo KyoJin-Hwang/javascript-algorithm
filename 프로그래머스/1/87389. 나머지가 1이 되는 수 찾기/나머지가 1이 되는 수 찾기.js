016c50a1840cdfc38efaa71e50836d91c8fdea6d
Owen <owen1127@naver.com>
2025-05-25 15:20:41 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 8.00 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
@@ -1,8 +1,7 @@
 function solution(n) {
-    let answer = 0;
-    for(let i = 1; i<=n; i++){
-        if(n % i === 1) return answer = i
-       
+    for (let x = 1; x < n; x++) {
+        if (n % x === 1) {
+            return x;
+        }
     }
-    return answer;
 }
