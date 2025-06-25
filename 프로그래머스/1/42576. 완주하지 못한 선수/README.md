3b4b247ecef798ea20d544d06a31352d5e7e1c8f
Owen <owen1127@naver.com>
2025-06-25 16:21:09 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 62.05 ms, Memory: 57.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=javascript) 
 
 ### 성능 요약
 
-메모리: 60.3 MB, 시간: 95.48 ms
+메모리: 57.8 MB, 시간: 62.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 16일 22:05:48
+2025년 06월 25일 16:21:00
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,11 +1,14 @@
 function solution(participant, completion) {
-    participant.sort(); 
-    completion.sort();
-    for(var i=0;i<participant.length;i++){
-        if(participant[i] !== completion[i]){
-            //인덱스 0부터 순차적으로 두 배열 비교
-            return participant[i];
-            //비완주자가 참가자 배열에 나올 경우 출력
-        }
+    let map = new Map();
+    for(let name of participant){
+        map.set(name, (map.get(name) || 0) + 1)
+    }
+    
+    for(let name of completion){
+        map.set(name, map.get(name) - 1)
+    }
+    
+    for(let [name, count] of map){
+        if(count > 0) return name
     }
 }
