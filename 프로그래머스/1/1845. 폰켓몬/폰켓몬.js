6ca614b8a008a01a69aa8a7de92dfe70305dcd42
Owen <owen1127@naver.com>
2025-06-27 17:15:21 +0900

    [level 1] Title: 폰켓몬, Time: 0.76 ms, Memory: 34.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,10 +1,5 @@
-const solution = nums => {
-    let answer = [];
-    const max = nums.length / 2;
-    nums.map((num) => {
-        answer.length < max && !answer.includes(num) ? answer.push(num) : num
-    });
-    
-    
-    return answer.length;    
+function solution(nums) {
+  const set = new Set(nums); 
+  const maxPick = nums.length / 2; // 고를 수 있는 최대 수
+  return Math.min(set.size, maxPick); 
 }
