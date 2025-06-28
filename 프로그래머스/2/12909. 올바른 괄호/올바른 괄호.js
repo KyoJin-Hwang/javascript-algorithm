1b6378709ad626c79795b6565642e67a63b5fe91
Owen <owen1127@naver.com>
2025-06-28 23:58:28 +0900

    [level 2] Title: 올바른 괄호, Time: 30.66 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,16 +1,18 @@
-function solution(s, answer){
-    let stack = 0
-    for(let char of s){
-        if(char === "("){
-            stack++
-        }else{
-            stack--
-        }
-         if (stack < 0) {
-            return false;
-        }
-        
-    }
+function solution(s) {
+  const stack = [];
 
-    return !stack;
+  for (const char of s) {
+    if (char === '(') {
+      stack.push(char); // 여는 괄호면 스택에 넣기
+    } else { // char === ')'
+      if (stack.length === 0) {
+        // 닫는 괄호인데 스택이 비어있으면 올바르지 않음
+        return false;
+      }
+      stack.pop(); // 짝 맞춰서 여는 괄호 하나 빼기
+    }
+  }
+
+  // 모든 문자열을 다 처리한 후 스택이 비어 있어야 올바른 괄호
+  return stack.length === 0;
 }
