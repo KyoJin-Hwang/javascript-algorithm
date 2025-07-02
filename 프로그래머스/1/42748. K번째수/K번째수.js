ca0d941f4b12bf5de79f844d1e5258322e473af3
Owen <owen1127@naver.com>
2025-07-02 18:03:49 +0900

    [level 1] Title: K번째수, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -10,3 +10,12 @@ function solution(array, commands) {
 
   return answer;
 }
+
+// function solution(array, commands) {
+//     var answer = [];
+
+//     answer = commands.map(a=>{
+//         return array.slice(a[0]-1,a[1]).sort((b,c)=>b-c)[a[2]-1];
+//     })
+//     return answer;
+// }
