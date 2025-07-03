ce2caafcfd9ab8e1d7876e42fd8c60f15e8cd88f
Owen <owen1127@naver.com>
2025-07-03 17:33:05 +0900

    [level 1] Title: 내적, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.js
+++ 프로그래머스/1/70128. 내적/내적.js
@@ -1,15 +1,4 @@
 function solution(a, b) {
-    let answer = 0;
-    // for문 활용 ( 가장 빠름 )
-    for(let i = 0; i < a.length; i++){
-        answer += a[i] * b[i]
-    }
-    return answer;
     
-    // map 활용
-    // a.map((el,idx) => answer += el * b[idx] )
-    // return answer;
-    
-    // reduce 활용
-    // return a.reduce((acc,cur,idx) => acc + (cur * b[idx]), 0)
+    return a.reduce((acc,cur,i)=> acc+(cur*b[i]),0);
 }
