ee1f06cd0eff3e7de1938ca1c96e32c491068bd7
Owen <owen1127@naver.com>
2025-07-03 17:26:37 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,5 +1,11 @@
 function solution(s) {
-    const mid = Math.floor(s.length/2);
-    
-    return s.length % 2 === 1 ? s[mid] : s[mid-1]+s[mid];
+  const mid = Math.floor(s.length / 2);
+
+  if (s.length % 2 === 0) {
+    // 짝수면 가운데 2글자
+    return s.slice(mid - 1, mid + 1);
+  } else {
+    // 홀수면 가운데 1글자
+    return s.charAt(mid);
+  }
 }
