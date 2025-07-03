8d58caa7cdcfd213c8e92bd166d5f46ff812082b
Owen <owen1127@naver.com>
2025-07-04 01:34:47 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,16 +1,6 @@
 function solution(n, arr1, arr2) {
-    let result = Array.from({length:n}, () => Array(n).fill(0));
-    arr1 = arr1.map((el)=> el.toString(2).padStart(n, 0).split(""))
-    arr2 = arr2.map((el)=> el.toString(2).padStart(n, 0).split(""))
-    console.log(result)
-    for(let i = 0; i < result.length; i++){
-        for(let j = 0; j < result.length; j++){
-            if(+arr1[i][j] === 1 || +arr2[i][j] === 1) {
-                result[i][j] = 1
-            }else{
-                result[i][j] = 0
-            }
-        }
-    }
-    return result.map((el)=> el.map((el2)=> el2 ? '#' : ' ').join(''))
+    return arr1.map((a, i) => {
+  const line = (a | arr2[i]).toString(2).padStart(n, '0');
+  return [...line].map(c => (c === '1' ? '#' : ' ')).join('');
+});
 }
