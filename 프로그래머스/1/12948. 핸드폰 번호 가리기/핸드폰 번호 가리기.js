803fb859aafb4064e3787583fa3805b65da313cd
Owen <owen1127@naver.com>
2025-07-03 16:40:54 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,11 +1,5 @@
-// function solution(phone_number) {
-//     const str = phone_number.slice(0, -4);
-    
-//     return phone_number.replace(str, '*'.repeat(str.length));
-// }
-
-// 코드 줄임 
-function solution(s){
-    let result = "*".repeat(s.length - 4) + s.slice(-4);
-    return result;
-  }
+function solution(phone_number) {
+  const lastFour = phone_number.slice(-4); 
+  const stars = '*'.repeat(phone_number.length - 4);
+  return stars + lastFour;
+}
