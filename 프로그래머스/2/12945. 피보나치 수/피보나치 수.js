fec8e22ccb48682f22dc7b61551b23d51f9fd7b0
Owen <rywls4248@gmail.com>
2025-09-07 16:05:28 +0900

    [level 2] Title: 피보나치 수, Time: 5.16 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -1,9 +1,9 @@
 function solution(n) {
-  const mod = 1234567;
+  const MOD = 1234567;
   let fib = [0, 1];
 
   for (let i = 2; i <= n; i++) {
-    fib[i] = (fib[i-1] + fib[i-2]) % mod;
+    fib[i] = (fib[i - 1] + fib[i - 2]) % MOD;
   }
 
   return fib[n];
