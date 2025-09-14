fdff85de47f4883417f1af20a96338b4cb5e418e
Owen <rywls4248@gmail.com>
2025-09-14 14:39:28 +0900

    [level 0] Title: 짝수의 합, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.js
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.js
@@ -1,14 +1,7 @@
-const solution = (n) => {
-    let sum = 0
-    for(let i=0; i<=n; i+=2) {
-        sum += i
+function solution(n) {
+    let answer = 0;
+    for(let i = 2; i <= n; i+=2){
+        answer += i
     }
-        
-    return sum
+    return answer;
 }
-
-// 설명
-// 1. sum 이라는 0을 선언해준다.
-// 2. for문으로 i = 0 부터 +=2 로 짝수 가 될 수 있게 2씩 더해준다. 
-// 3. sum += i ( 이전의 sum 값과 +=2 된 수를 더해준다.)
-// 4. 마지막 for 문끝나고 sum return 해준다. 
