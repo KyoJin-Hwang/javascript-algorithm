1776d232d45d713aa19c9454af57ea3427aa6fef
Owen <rywls4248@gmail.com>
2025-09-14 15:06:58 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.js
@@ -1,11 +1,4 @@
 function solution(n) {
-  return n
-    .toString()
-    .split("")
-    .reduce((acc, cur) => acc + Number(cur), 0);
+    let answer = n.toString().split('').reduce((acc,cur)=> acc + +cur,0)
+    return answer;
 }
-
-// 설명 
-// 1234 Number type을 string 으로 타입 변환 
-// split 으로 배열 나눔 
-// reduce 에서 cur 을 Number type으로 변화하여 누적된값을 더해서 return 
