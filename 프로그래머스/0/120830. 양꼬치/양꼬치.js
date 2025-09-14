47165a0d9bd140d351d1c873b8d8893ad403b36a
Owen <rywls4248@gmail.com>
2025-09-14 14:38:09 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.js
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.js
@@ -1,9 +1,9 @@
 function solution(n, k) {
     let answer = 0;
-    let sheep = 12000
+    let lamb = 12000
     let drink = 2000
     let service = Math.floor(n / 10)
     
-    answer = (sheep * n) + (drink * (k-service))
+    answer = (lamb * n) + (drink * (k-service))
     return answer;
 }
