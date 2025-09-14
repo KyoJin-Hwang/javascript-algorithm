c51b0a783acf19286c4d2b148e0c2ca49dbe8059
Owen <rywls4248@gmail.com>
2025-09-14 14:45:08 +0900

    [level 0] Title: 배열의 평균값, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
@@ -1,5 +1,11 @@
-const solution = (numbers) => numbers.reduce((acc, cur)=> acc+cur)/numbers.length 
-
-// 설명
-// 1. reduce 함수로 배열에 있는것을 다 더해준다. 
-// 2. numbers 의 길이만큼 나눠준다. 
+function solution(numbers) {    
+    let avg = numbers.reduce((acc, cur, idx, arr) => {
+    acc += cur; 
+    if (idx === arr.length - 1) {
+      return acc / arr.length; // 마지막 요소에서 평균 반환
+    }
+    return acc;
+  }, 0);
+    
+    return avg;
+}
