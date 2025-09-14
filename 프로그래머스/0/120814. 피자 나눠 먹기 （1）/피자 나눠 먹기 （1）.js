2f6cf19ca7afdea620dc72ff4b38f73434d5adba
Owen <rywls4248@gmail.com>
2025-09-14 14:55:11 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
@@ -1,8 +1,13 @@
-function solution(n) {
-    let answer = 0
-    
-    for(let i = n; i >= 1; i-=7){
-        answer++
-    }
-    return answer;
+function solution(n){
+    return Math.ceil(n/7) 
 }
+
+// 내풀이 
+// function solution(n) {
+//     let answer = 0
+    
+//     for(let i = n; i >= 1; i-=7){
+//         answer++
+//     }
+//     return answer;
+// }
