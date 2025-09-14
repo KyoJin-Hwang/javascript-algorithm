dcce1b3d99ca8dc671cfc7fb8c5c8ce4156bd000
Owen <rywls4248@gmail.com>
2025-09-14 14:37:24 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.js
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.js
@@ -1,17 +1,9 @@
-
 function solution(n, k) {
+    let answer = 0;
+    let sheep = 12000
+    let drink = 2000
+    let service = Math.floor(n / 10)
     
-    return n*12000 + k*2000 - parseInt(n/10)*2000
+    answer = (sheep * n) + (drink * (k-service))
+    return answer;
 }
-
-// 설명
-// 1. 양꼬치 인분수와 가격을 곱한다 
-// 2. 콜라의 갯수와 가격을 곱한다
-// 3. 양꼬치와 콜라의 가격을 더하고 마지막으로 서비스콜라 금액만큼 빼서 총 결제금액을 return 한다 
-
-// Method
-// 1. parseInt : 소수점은 정수만 사용하고, String을 Number로 받기위해서 사용, 소수점 방지  
-
-// ~~ 사용 가능 
-// 장점은 성능이 좋아진다.
-// 단점은 코드가 길어질때 협업이 힘들다 
