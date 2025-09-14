361237eeb8cc57a4b6ca50c8fe9fbc1de163ae5c
Owen <rywls4248@gmail.com>
2025-09-14 14:58:24 +0900

    [level 0] Title: 세균 증식, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120910. 세균 증식/세균 증식.js
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.js
@@ -1,6 +1,10 @@
 function solution(n, t) {    
-    for(let i = 1; i <= t; i++){
-        n *= 2
-    }
-    return n;
+    return n * Math.pow(2, t)
 }
+// 내풀이
+// function solution(n, t) {    
+//     for(let i = 1; i <= t; i++){
+//         n *= 2
+//     }
+//     return n;
+// }
