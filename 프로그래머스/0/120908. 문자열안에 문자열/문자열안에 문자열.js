9a6a5f8cbf06b986c5930f30892234d0e92a6804
Owen <rywls4248@gmail.com>
2025-09-15 17:00:31 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.js
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.js
@@ -1,6 +1,3 @@
 function solution(str1, str2) {
-    return str1.indexOf(str2) === -1 ? 2 : 1;
+    return str1.indexOf(str2) !== -1 ? 1 : 2
 }
-
-// 설명
-// indexOf로 str1을 검수해 str2을 포함한다면 1 포함하지않는다면 2로 삼항연산자 
