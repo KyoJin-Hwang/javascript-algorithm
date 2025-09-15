114ca8a6f7ec617f8096840b7af8fbf870fe24a4
Owen <rywls4248@gmail.com>
2025-09-15 17:06:56 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.js
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.js
@@ -1,7 +1,5 @@
 function solution(n) {
-    return Math.sqrt(n) % 1 === 0 ? 1 : 2;
+    let sqrt = Math.sqrt(n)
+    let answer = n % sqrt === 0 ? 1 : 2    
+    return answer;
 }
-
-// 설명
-// 1. Math.sqrt 함수를 이용하여 n의 제곱을 구해준다. 
-// 2. 삼항연산자를 사용하여 1에서 구한 제곱을 % 1 하여 나머지가 0이 되면 1 아니면 2로 return 해준다.
