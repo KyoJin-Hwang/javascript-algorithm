b1d017e8fbeb7e82b6f3e78b4ef42fdebbb90e3b
Owen <rywls4248@gmail.com>
2025-09-15 16:00:19 +0900

    [level 0] Title: 배열의 유사도, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,10 +1,19 @@
+// 내 풀이
+// function solution(s1, s2) {
+//     let answer = 0    
+//     for(let i = 0; i < s1.length; i++){
+//         if(s2.includes(s1[i])){
+//             answer++
+//         }
+//     }
+//     return answer
+// }
+
+
+3
+4
+5
 function solution(s1, s2) {
-    let answer = 0
-    
-    for(let i = 0; i < s1.length; i++){
-        if(s2.includes(s1[i])){
-            answer++
-        }
-    }
-    return answer
+    const intersection = s1.filter((x) => s2.includes(x));
+    return intersection.length;
 }
