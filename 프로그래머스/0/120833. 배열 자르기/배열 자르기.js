47c723115fbde13b50ab310ba4586371471213d0
Owen <rywls4248@gmail.com>
2025-09-15 15:36:47 +0900

    [level 0] Title: 배열 자르기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120833. 배열 자르기/배열 자르기.js
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.js
@@ -1,6 +1,4 @@
 function solution(numbers, num1, num2) {
-    return numbers.slice(num1,num2+1);
+    
+    return numbers.slice(num1, num2+1);
 }
-
-// 설명
-// 1. slice 배열함수를 사용하여 num1 부터 num2+1 의 이전 값 전까지 배열을 잘라준다. 
