470a3c34655dbef6218d58548343d6cd6a08b099
Owen <rywls4248@gmail.com>
2025-09-15 16:44:24 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.js
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.js
@@ -1,6 +1,7 @@
 function solution(strlist) {
-    return strlist.map(el => el.length)
+    let answer = [];
+    for(let str of strlist){
+        answer.push(str.length)
+    }
+    return answer;
 }
-
-// 설명
-// map 으로 strlist 를 순회하여 각 el의 length 만큼의 값으로 변경해준다
