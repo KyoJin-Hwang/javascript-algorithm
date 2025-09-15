91d190773c966412aca28f9a43b23cc9cdb1e9fc
Owen <rywls4248@gmail.com>
2025-09-15 16:43:19 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.js
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.js
@@ -1,9 +1,3 @@
 function solution(n, numlist) {
-    let answer = [];
-    answer = numlist.filter((item)=> item % n === 0)
-    return answer;
+    return numlist.filter((el)=> el % n === 0)
 }
-
-// 설명 
-// filter 함수를 활용하여 numlist 를 순회하여 나눈 나머지가 0 인것만 
-// answer 빈배열에 넣어 return 한다.
