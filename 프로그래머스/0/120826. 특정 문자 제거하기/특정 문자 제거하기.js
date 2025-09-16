811ba81ca715c1794944f262755f1f2edf7d2953
Owen <rywls4248@gmail.com>
2025-09-16 21:40:09 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -1,5 +1,10 @@
+// 내 풀이
+// function solution(my_string, letter) {
+//     let answer = [...my_string].filter((el)=> el !== letter).join('')
+//     return answer;
+// }
+
 function solution(my_string, letter) {
-    let answer = [...my_string].filter((el)=> el !== letter).join('')
-    
+    const answer = my_string.split(letter).join('')
     return answer;
 }
