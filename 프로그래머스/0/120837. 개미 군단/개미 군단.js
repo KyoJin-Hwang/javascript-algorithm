5fd30dd05ff0108b8e20626dddba26bce3c8b98a
Owen <rywls4248@gmail.com>
2025-09-20 12:41:05 +0900

    [level 0] Title: 개미 군단, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.js
@@ -1,6 +1,6 @@
 function solution(hp) {
   const first = Math.floor(hp / 5);
-  const second = Math.floor((hp - first * 5) / 3);
-  const third = hp - first * 5 - second * 3;
+  const second = Math.floor((hp % 5) / 3);
+  const third = (hp % 5) % 3;
   return first + second + third;
 }
