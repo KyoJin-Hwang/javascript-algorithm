7d33a0c40f3331034f6a09d03f1672f60d380db9
Owen <rywls4248@gmail.com>
2025-09-22 20:53:50 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -1,3 +1,3 @@
 function solution(my_string) {
-    return [...my_string].filter((el)=> !isNaN(el)).map((el)=> Number(el)).sort((a,b)=> a-b)
+    return my_string.match(/\d/g).sort((a, b) => a - b).map(n => Number(n));
 }
