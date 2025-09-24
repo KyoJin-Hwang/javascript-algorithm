d412c52465b5db015487974b4c44c25a4c2f640b
Owen <rywls4248@gmail.com>
2025-09-24 18:34:52 +0900

    [level 0] Title: 공배수, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181936. 공배수/공배수.js
+++ 프로그래머스/0/181936. 공배수/공배수.js
@@ -1,5 +1,4 @@
 function solution(number, n, m) {
-    let answer = 0;
-    if(number % n === 0 && number % m === 0) return answer += 1
-    return answer;
+    
+    return number % n === 0 && number % m === 0 ? 1 : 0;
 }
