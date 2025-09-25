1dd0737b91c5dfdcbd994d4898ceca63f0bd1684
Owen <rywls4248@gmail.com>
2025-09-25 17:34:56 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
@@ -1,14 +1,4 @@
 function solution(num_list) {
-    return num_list.findIndex(v => v < 0)
+    let answer = 0;
+    return num_list.findIndex((el)=> el < 0);
 }
-
-// for 문 풀이 
-
-// function solution(num_list) {
-//     for (i = 0; i < num_list.length; i++) {
-//         if (num_list[i] < 0) {
-//             return i
-//         } 
-//     }
-//     return -1
-// }
