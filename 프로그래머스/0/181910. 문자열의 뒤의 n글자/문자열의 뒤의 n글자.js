87930914f69dc6a7f906776b8febb04bdafe867b
Owen <rywls4248@gmail.com>
2025-09-27 22:00:25 +0900

    [level 0] Title: 문자열의 뒤의 n글자, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.js
+++ 프로그래머스/0/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.js
@@ -1,7 +1,4 @@
 function solution(my_string, n) {
-    
-    return my_string.slice(-n);
+    let answer = my_string.slice(my_string.length-n);
+    return answer;
 }
-
-// 설명 
-// slice 함수를 활용하여 -n 부터 값을 불러온다. 
