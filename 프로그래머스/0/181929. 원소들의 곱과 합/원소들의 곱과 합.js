1de6b500bb7e0b2858d23e6de903badac8231f87
Owen <rywls4248@gmail.com>
2025-09-28 20:18:45 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.js
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.js
@@ -1,10 +1,6 @@
 function solution(num_list) {
-    let mul = num_list.reduce((a, c) => a*c, 1);
-    let sum = num_list.reduce((a, c) => a+c, 0);
-    return (mul < sum*sum) ? 1 : 0;
+    const a = num_list.reduce((acc,cur)=> acc * cur, 1)
+    const b = Math.pow(num_list.reduce((acc,cur)=> acc + cur, 0), 2)    
+    
+    return a < b ? 1 : 0;
 }
-
-// 설명
-// reduce mul 과 sum 을 선언하여 계산해준다 
-// sum 은 더해야하므로 0 부터 mul 은 * 가 있으므로 1부터 시작한다
-// 마지막 return 에서 mul 이 sum*sum 보다 작을 경우 1 클경우는 0으로한다. 
