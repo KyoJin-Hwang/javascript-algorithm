d64c55ca0c65ca8d1ee87362c5dbcf8d26fd70f0
Owen <rywls4248@gmail.com>
2025-09-28 20:13:22 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -1,7 +1,3 @@
 function solution(my_string, is_suffix) {
-    
-    return +my_string.endsWith(is_suffix)
+    return my_string.slice(my_string.length - is_suffix.length) === is_suffix ? 1 : 0
 }
-
-// 설명
-// endsWith로 해당문자열로 끝나는지 확인가능 
