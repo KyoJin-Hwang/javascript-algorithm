900b8ee7f7eaee13a522d89dac9f9fdd9ef68405
Owen <rywls4248@gmail.com>
2025-09-29 18:51:00 +0900

    [level 0] Title: 두 수의 연산값 비교하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.js
+++ 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.js
@@ -1,8 +1,6 @@
 function solution(a, b) {
-    let num1 = parseInt(a+""+b);
-    let num2 = 2*a*b;
-    return num1 > num2 ? num1 : num2;
+    let answer = 0;
+    let str = +`${a}${b}`
+    answer = 2 * a * b
+    return str > answer ? str : answer;
 }
-
-// 설명
-// parseInt 로 정수만 반환하고 빈값을 추가한 이유는 a+b 가 되어 93이 나올수 있기떄문에 
