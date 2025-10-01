32d635417e68bf701755847abcf58d944f486071
Owen <rywls4248@gmail.com>
2025-10-01 22:25:36 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -3,7 +3,3 @@ function solution(my_string, num1, num2) {
     [my_string[num1], my_string[num2]] = [my_string[num2], my_string[num1]];
     return my_string.join('');
 }
-
-// 설명
-// split 으로 배열을 쪼개준다. 
-// 배열의 구조분해할당으로 값을 변경해준다. 
