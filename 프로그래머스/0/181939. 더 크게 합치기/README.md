691f34919a5d926cd1640eb2996571c8df3c78b8
Owen <rywls4248@gmail.com>
2025-10-12 22:24:30 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/README.md
+++ 프로그래머스/0/181939. 더 크게 합치기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 더 크게 합치기 - 181939 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181939) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181939?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.04 ms
+메모리: 33.4 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 25일 20:45:27
+2025년 10월 12일 22:24:27
 
 ### 문제 설명
 
--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
@@ -1,10 +1,6 @@
 function solution(a, b) {
-    const num1 = parseInt(a+""+b)
-    const num2 = parseInt(b+""+a)
+    const result1 = String(a)+String(b)
+    const result2 = String(b)+String(a)
     
-    return num1 >= num2 ? num1 : num2;
+    return result1>=result2 ? Number(result1): Number(result2)
 }
-
-// 설명 
-// parseInt 로 a b, b a 를 합쳐준다.  "" 빈값은 서로 더해지는 오류를 해결하기위함 
-// 삼항연사자로 더 높은것을 return 
