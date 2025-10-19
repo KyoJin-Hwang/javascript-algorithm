0c98052c029ccc4cb1d89f2c6ad6434153cad5f3
Owen <rywls4248@gmail.com>
2025-10-19 20:46:19 +0900

    [level 0] Title: 약수 구하기, Time: 0.49 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.js
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.js
@@ -1,8 +1,8 @@
 function solution(n){
     let arr =[]
-    for(let i=1; i<=n; i++){
-        if(n%i===0){
-            arr.push(i)
+    for(divisor=1; divisor<=n; divisor++){
+        if(n%divisor===0){
+            arr.push(divisor)
         }
     }
     return arr
