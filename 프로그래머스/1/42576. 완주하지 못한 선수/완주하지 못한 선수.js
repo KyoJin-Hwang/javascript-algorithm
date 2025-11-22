9ace050fc33d6e921c5b319b1f7a7f45a57b21e0
Owen <rywls4248@gmail.com>
2025-11-22 11:53:08 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 53.90 ms, Memory: 64.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,12 +1,18 @@
 function solution(participant, completion) {
-    let map = new Map();
-    for(let name of participant){
-        map.set(name, (map.get(name) || 0) + 1)
-    }
-    for(let name of completion){
-        map.set(name, map.get(name)-1)
-    }
-    for(let [name, count] of map){
-        if(count > 0) return name
-    }
+  const map = new Map();
+
+  // 1) 참가자 이름 세기
+  participant.forEach(name => {
+    map.set(name, (map.get(name) || 0) + 1);
+  });
+
+  // 2) 완주자 이름 빼기
+  completion.forEach(name => {
+    map.set(name, map.get(name) - 1);
+  });
+
+  // 3) 값이 1 남아 있는 사람 찾기
+  for (const [name, count] of map) {
+    if (count > 0) return name;
+  }
 }
